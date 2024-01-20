495cff70e0b1f7c9fe6537c9f88b940f3e7c0de4
tasib94 <rahi84974@gmail.com>
2024-01-20 22:00:43 +0600

    got the track

--- js/phtube.js
+++ js/phtube.js
@@ -1,20 +1,22 @@
 const loadVideos = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
-    const data = await res.json();
-    const videos = data.data;
-    // console.log(videos);
-    displayVideos(videos);
+  const res = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
+  const data = await res.json();
+  const videos = data.data;
+  // console.log(videos);
+  displayVideos(videos);
 }
 
+const videoContainer = document.getElementById('videos-container')
+
 const displayVideos = videos => {
-    // console.log(videos)
-    videos.forEach(video => {
-        console.log(video)
-        // create a div
-        const videoCard = document.createElement('div')
-        videoCard.classList = `card w-96 bg-base-100 shadow-xl`
-        videoCard.innerHTML = `
-        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  // console.log(videos)
+  videos.forEach(video => {
+    console.log(video)
+    // create a div
+    const videoCard = document.createElement('div')
+    videoCard.classList = `card w-96 bg-base-100 shadow-xl`
+    videoCard.innerHTML = `
+        <figure><img src="${video.thumbnail}" alt="Shoes" /></figure>
                     <div class="card-body">
                       <h2 class="card-title">
                           <div class="badge badge-secondary">NEW</div>
@@ -26,9 +28,10 @@ const displayVideos = videos => {
                         <div class="badge badge-outline">Products</div>
                       </div>
                     </div>
-        `
-        
-    });
+        `;
+        videoContainer.appendChild(videoCard);
+
+  });
 
 }
 
